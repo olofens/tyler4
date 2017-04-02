6f8ad601abad61a8180a1fca10c4a0e5e90ac311
Strid <stride@student.chalmers.se>
2017-04-02 11:41:53 +0200

    add tiledmap world and simple controller

--- miniMinerDir/core/src/com/mygdx/game/Screens/PlayScreen.java
+++ miniMinerDir/core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MiniMiner;
@@ -16,15 +19,24 @@ import com.mygdx.game.MiniMiner;
 public class PlayScreen implements Screen {
 
     private MiniMiner game;
-    Texture texture;
     private OrthographicCamera gameCam;
     private Viewport viewPort;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
+
     public PlayScreen(MiniMiner game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         gameCam = new OrthographicCamera();
-        viewPort = new FitViewport(800,480, gameCam);
+        viewPort = new FitViewport(480,800, gameCam);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("tmxtest.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(viewPort.getWorldWidth()/2, 1400, 0);
     }
 
     @Override
@@ -32,14 +44,34 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()){
+            gameCam.position.x += 200 * dt;
+        }
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+
+
         game.batch.setProjectionMatrix(gameCam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+        //game.batch.begin();
+        //game.batch.draw(texture, 0, 0);
+        //game.batch.end();
 
     }
 
