c3e831af52f3f9200891b9107b9913889abecc38
Omar Oueidat <omar9604@hotmail.comgit config --global user.email omar9604@hotmail.com>
2017-04-03 10:50:03 +0200

    Add StartMenuScreen

---
+++ miniMinerDir/core/src/com/mygdx/game/Screens/StartMenuScreen.java
@@ -0,0 +1,47 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Omaroueidat on 03/04/17.
+ */
+
+public class StartMenuScreen implements Screen {
+
+    
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
