90016a4dbcd83aa2cf11a1b545a141d9e8d0d1a5
gabrielwallin <walling@student.chalmers.se>
2017-04-10 15:41:13 +0200

    add template for test

---
+++ miniMinerDir/core/src/test/java/com/mygdx/game/Screens/PlayScreenTest.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.Screens;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by walling on 2017-04-10.
+ */
+public class PlayScreenTest {
+    @Test
+    public void dispose() throws Exception {
+
+    }
+
+}
