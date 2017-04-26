28ff1264095523faad842a6290af4e9e8d882ca9
Olof Enström <olofens@student.chalmers.se>
2017-04-26 20:29:47 +0200

    add minerWorldContactListener class

---
+++ miniMinerDir/core/src/com/mygdx/game/Tools/MinerWorldContactListener.java
@@ -0,0 +1,35 @@
+package com.mygdx.game.Tools;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/**
+ * Created by Olof Enström on 2017-04-26.
+ */
+
+public class MinerWorldContactListener implements ContactListener {
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture a = contact.getFixtureA();
+        Fixture b = contact.getFixtureB();
+        if ()
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
