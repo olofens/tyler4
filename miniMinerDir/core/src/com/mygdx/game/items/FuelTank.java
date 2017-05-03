b220079809409ae710dc788c097999e68a3b7f52
Omar Oueidat <oueidat@student.chalmers.se>
2017-05-03 23:57:10 +0200

    Add javadoc

--- miniMinerDir/core/src/com/mygdx/game/items/FuelTank.java
+++ miniMinerDir/core/src/com/mygdx/game/items/FuelTank.java
@@ -8,13 +8,25 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 /**
  * Created by Omaroueidat on 03/05/17.
  */
+
+/**
+ * The class that dictates the fuel for the miner
+ */
 public class FuelTank implements IResource {
 
+    /**
+     * The fuel variable
+     */
+    private Integer fuel;
 
-    public Integer fuel;
+    /**
+     * Fuel label
+     */
+    private Label fuelLabel;
 
-    public Label fuelLabel;
-
+    /**
+     * Constructor which gives our default fuel ammount and makes our label with a red color and a certain position
+     */
     public FuelTank() {
         fuel = 100000;
 
@@ -25,6 +37,10 @@ public class FuelTank implements IResource {
         fuelLabel.setText(strFuel.substring(0,fuelLength-3) + "%");
     }
 
+    /**
+     * Method that adjusts the fuel when the miner is moving
+     * @param knobPercent
+     */
     public void adjustFuel(Integer knobPercent){
         fuel = fuel - Math.abs(3*knobPercent);
 
@@ -35,10 +51,18 @@ public class FuelTank implements IResource {
         fuelLabel.setText(strFuel.substring(0,fuelLength-3) + "%");
     }
 
+    /**
+     *
+     * @return fuel
+     */
     public Integer getFuel() {
         return fuel;
     }
 
+    /**
+     * A lable getter
+     * @return fuelLabel
+     */
     public Label getFuelLabel() {
         return fuelLabel;
     }
--- miniMinerDir/core/src/com/mygdx/game/items/Hull.java
+++ miniMinerDir/core/src/com/mygdx/game/items/Hull.java
@@ -9,20 +9,42 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 /**
  * Created by Omaroueidat on 03/05/17.
  */
+
+/**
+ * The class that dictates the health of the miner
+ */
 public class Hull implements IResource {
-    public Label hullLabel;
 
-    public Integer hull;
+    /**
+     * Label for the health
+     */
+    private Label hullLabel;
 
+    /**
+     * Variable for the ammount of health
+     */
+    private Integer hull;
+
+    /**
+     * Constructor which gives our default value for the health and creates a label;
+     */
     public Hull(){
         hull = 100;
         hullLabel = new Label(String.format("%03d", hull), new Label.LabelStyle(new BitmapFont(), com.badlogic.gdx.graphics.Color.GREEN));
     }
 
+    /**
+     *
+     * @return hullLabel
+     */
     public Label getHullLabel() {
         return hullLabel;
     }
 
+    /**
+     *
+     * @return hull
+     */
     public Integer getHull() {
         return hull;
     }
