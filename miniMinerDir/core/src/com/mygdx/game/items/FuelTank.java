95b1ef1adf8184a5ec2c6039e29f99497648c6d2
Omar Oueidat <oueidat@student.chalmers.se>
2017-05-03 23:15:09 +0200

    add Fuel class

---
+++ miniMinerDir/core/src/com/mygdx/game/items/FuelTank.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.items;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.sun.tools.javac.util.Name;
+
+import java.awt.*;
+
+/**
+ * Created by Omaroueidat on 03/05/17.
+ */
+public class FuelTank {
+
+    public Integer fuel;
+
+    private Label fuelLabel;
+    
+}
