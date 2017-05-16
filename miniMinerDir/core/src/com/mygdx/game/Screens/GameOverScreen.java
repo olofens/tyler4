b994dc2cba59d7db3f690dbb9d5824eb5ebbf18e
Strid <stride@student.chalmers.se>
2017-05-17 00:51:33 +0200

    minor changes

--- miniMinerDir/core/src/com/mygdx/game/Screens/GameOverScreen.java
+++ miniMinerDir/core/src/com/mygdx/game/Screens/GameOverScreen.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -31,22 +35,25 @@ import com.mygdx.game.event.Shout;
 
 public class GameOverScreen implements Screen {
 
-    private MiniMiner game1;
     private Viewport viewport;
     private Stage stage;
+    Pixmap background;
+    private ShapeRenderer shapeRenderer;
 
+    private MiniMiner game1;
     private TextButton playAgainButton;
     private Table table;
     private Game game;
 
     public GameOverScreen(Game game) {
 
+        MiniMiner game1 = new MiniMiner();
+
 
         this.game = game;
         viewport = new FitViewport(Constants.V_WIDTH, Constants.V_HEIGHT, new OrthographicCamera());
 
         stage = new Stage(viewport, ((MiniMiner) game).batch);
-
 
 
         Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
@@ -57,16 +64,13 @@ public class GameOverScreen implements Screen {
         //table.setFillParent(true);
         Skin storeSkin = new Skin(Gdx.files.internal("skins/rusty-robot-ui.json"), new TextureAtlas(Gdx.files.internal("skins/rusty-robot-ui.atlas")));
 
-        //Label gameOverLabel = new Label("Game Over", font);
-
 
         playAgainButton = new TextButton("Play again", storeSkin);
         playAgainButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Button Pressed");
+                createPlayScreen();
             }
-
         });
         table = new Table(storeSkin);
         table.center();
@@ -76,6 +80,11 @@ public class GameOverScreen implements Screen {
         stage.addActor(table);
 
 
+
+    }
+
+    public void createPlayScreen(){
+        game.setScreen(new PlayScreen((MiniMiner)game));
     }
 
     @Override
@@ -85,10 +94,29 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(new Color(0.1f, 0, 0, 0.5f));
+        shapeRenderer.rect(0, 0, Constants.V_WIDTH, Constants.V_HEIGHT);
+        shapeRenderer.end();
+
 
         stage.act();
         stage.draw();
+/*
+        shapeRenderer = new ShapeRenderer();
 
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(0, 0, 0, 0.5f);
+        shapeRenderer.rect(0, 0, Constants.V_WIDTH, Constants.V_HEIGHT);
+        shapeRenderer.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+        */
     }
 
     @Override
--- miniMinerDir/core/src/com/mygdx/game/items/FuelTank.java
+++ miniMinerDir/core/src/com/mygdx/game/items/FuelTank.java
@@ -53,7 +53,7 @@ public class FuelTank implements IGear {
     }
 
     public boolean isEmpty(){
-        if(fuel <= 98000){
+        if(fuel <= 95000){
             return true;
         }
         return false;
