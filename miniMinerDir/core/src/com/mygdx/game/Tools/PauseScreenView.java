1e99d926da264c657e9d10d2db0bc0445ab07304
Olof Enström <olofens@student.chalmers.net>
2017-05-25 17:11:01 +0200

    split pausescreenhandler into view and controller

---
+++ miniMinerDir/core/src/com/mygdx/game/Tools/PauseScreenView.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.Tools;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by olofenstrom on 2017-05-25.
+ */
+
+public class PauseScreenView {
+    private TextButton resumeButton;
+    private TextButton menuButton;
+
+    public PauseScreenView() {
+        Skin storeSkin = new Skin(Gdx.files.internal("skins/rusty-robot-ui.json"),
+                new TextureAtlas(Gdx.files.internal("skins/rusty-robot-ui.atlas")));
+
+        resumeButton = new TextButton("Resume", storeSkin);
+        menuButton = new TextButton("Main Menu", storeSkin);
+    }
+
+    public TextButton getResumeButton() {
+        return resumeButton;
+    }
+
+    public TextButton getMenuButton() {
+        return menuButton;
+    }
+}
